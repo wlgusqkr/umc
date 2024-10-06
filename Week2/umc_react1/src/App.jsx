b171acd4cabebedb5eed0e39e23b192c47aef440
unknown <wlgusqkr11@naver.com>
2024-10-07 08:24:43 +0900

    Week2

---
+++ Week2/umc_react1/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css'
+import { MOVIES } from './mocks/movie';
+
+function App() {
+  const [but, setBut] = useState(false);
+
+  return (
+    <>
+      <img src='https://image.tmdb.org/t/p/original/wwemzKWzjKYJFfCeiB57q3r4Bcm.svg'></img>
+      <div style={{ display: 'flex', gap: '15px', width: '100%', flexWrap: 'wrap', }}>
+        {MOVIES.results.map((element, index) => {
+          return (
+            <div onMouseOver={()=>{setBut(index)}} style={{ position: 'relative' }}>
+              <img key={index} src={`https://image.tmdb.org/t/p/w200${element.poster_path}`} width={'170px'} height={'200px'}></img>
+              { but == index ? <div style={{ position: 'absolute', backgroundColor: 'black', opacity: '0.7', width: '170px', height: '200px', left: '0', top: '0'}}></div> : <></>}
+            </div>
+          )
+        })}
+      </div>
+    </>
+  )
+}
+
+export default App;
