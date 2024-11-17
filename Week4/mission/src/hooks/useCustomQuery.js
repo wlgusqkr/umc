b0588a886b482b0a67176b4e6cdb5378bb369710
박지현 <wlgusqkr11@naver.com>
2024-11-17 13:37:57 +0900

    Week7mission3끝

--- Week4/mission/src/hooks/useCustomQuery.js
+++ Week4/mission/src/hooks/useCustomQuery.js
@@ -1,11 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "../apis/axios-instance";
 
 const useCustomQuery = (url, key) => {
   const { data, isLoading, isError } = useQuery({
     queryKey: [key],
     queryFn: async () => (await axiosInstance.get(url)),
-    staleTime: 1000 * 10
+    staleTime: 1000 * 10,
+    placeholderData: keepPreviousData
   })
   return { data, isLoading, isError }
 }
--- Week4/mission/src/page/moviesbytype.jsx
+++ Week4/mission/src/page/moviesbytype.jsx
@@ -1,37 +1,14 @@
 import { useNavigate, useParams } from "react-router-dom";
-import styled, { keyframes } from "styled-components";
+import styled from "styled-components";
 import CardSkeletonList from "../component/Skeleton/card-skeleton-list";
-import { useEffect, useRef } from "react";
-import useCustomInfiniteQuery from "../hooks/useCustomInfiniteQuery";
+import { useState } from "react";
+import useCustomQuery from "../hooks/useCustomQuery";
 const MoviesByType = () => {
   const navigate = useNavigate();
   const params = useParams();
-  const url = `/movie/${params.movieType}?language=ko-KR`
-  const { data: movies, isLoading, isError, fetchNextPage, hasNextPage, isFetching } = useCustomInfiniteQuery(url, params.movieType);
-  const ref = useRef(null);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(async (entry) => {
-        if (entry.isIntersecting && hasNextPage) {
-          console.assert(fetchNextPage)
-          console.dir(fetchNextPage)
-          await fetchNextPage();
-        }
-      })
-    }, { threshold: 0.1 })
-    if (ref.current) {
-      observer.observe(ref.current)
-    }
-
-    return () => {
-      if (ref.current) {
-        observer.unobserve(ref.current);
-      }
-    }
-
-  }, [movies, fetchNextPage, hasNextPage]);
-  console.log(movies)
+  const [currentPage, setCurrentPage] = useState(1);
+  const url = `/movie/${params.movieType}?language=ko-KR&page=${currentPage}`
+  const { data: movies, isLoading, isError } = useCustomQuery(url, params.movieType + currentPage);
 
   if (isLoading === true) {
     return (
@@ -52,31 +29,27 @@ const MoviesByType = () => {
     <>
       <Container>
         {
-          movies.pages.map((dataPerMovies, Idx) => (
-            dataPerMovies.data.results?.map((element, index) => {
-              const isLastElement = index === dataPerMovies.data.results?.length - 1; // 항상 최신 배열 기반으로 계산
-              return (
-                <MovidCard ref={isLastElement ? ref : null} key={index} onClick={() => { navigate(`/movies/detail/${element.id}`) }}>
-                  <img src={`https://image.tmdb.org/t/p/w200${element.poster_path}`} style={{ width: '100%', height: '250px', borderRadius: '20px', objectFit: 'cover' }}></img>
-                  <p style={{ margin: '0', fontWeight: '600', padding: '4px 2px', fontSize: '12px' }}>{element.title}</p>
-                  <p style={{ margin: '0', fontSize: '12px', padding: '4px 0px' }}>{element.release_date}</p>
-                </MovidCard>
+          movies.data.results?.map((element, index) => {
+            return (
+              <MovidCard key={index} onClick={() => { navigate(`/movies/detail/${element.id}`) }}>
+                <img src={`https://image.tmdb.org/t/p/w200${element.poster_path}`} style={{ width: '100%', height: '250px', borderRadius: '20px', objectFit: 'cover' }}></img>
+                <p style={{ margin: '0', fontWeight: '600', padding: '4px 2px', fontSize: '12px' }}>{element.title}</p>
+                <p style={{ margin: '0', fontSize: '12px', padding: '4px 0px' }}>{element.release_date}</p>
+              </MovidCard>
 
-              )
-            })
-          ))
+            )
+          })
         }
-        {
-          isFetching &&
-          <>
-            <CardSkeletonList number={20} />
-            <SpinnerContainer>
-              <div>
-
-              </div>
-            </SpinnerContainer>
-          </>
-        }
+        <ButtonContainer>
+          {
+            currentPage === 1 ?
+              <button onClick={() => { setCurrentPage(prev => prev - 1) }} disabled style={{backgroundColor:'gray'}}>이전</button>
+              :
+              <button onClick={() => { setCurrentPage(prev => prev - 1) }}>이전</button>
+          }
+          <span> {currentPage} 페이지</span>
+          <button onClick={() => { setCurrentPage(prev => prev + 1) }}>다음</button>
+        </ButtonContainer>
       </Container>
     </>
   )
@@ -87,34 +60,28 @@ export default MoviesByType;
 const Container = styled.div`
   display: flex;
   flex-wrap: wrap;
-  justify-contents: space-evenly;
+  justify-content: space-evenly;
   gap: 20px;
 `
 const MovidCard = styled.div`
-  width: 15%;
+  width: 150px;
   display: flex;
   flex-direction: column;
 `
-const spin = keyframes`
-  0% {
-      transform: rotate(0deg);
-  }
-  100% {
-      transform: rotate(360deg);
-  }
 
-`
-const SpinnerContainer = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  div {
-    width: 70px;
-    height: 70px;
-    border: 7px solid rgba(163, 151, 198, 0.2);
-    border-top: 7px solid rgba(163, 151, 198, 1);
-    border-radius: 50%;
-    animation: ${spin} 2s linear infinite;
+
+const ButtonContainer = styled.div`
+  width: 100%; 
+  gap: 10px; 
+  display:flex; 
+  justify-content : center; 
+  align-items : center; 
+  margin : 15px ;
+  button {
+    color: black; 
+    width: 55px; 
+    height: 35px; 
+    border-radius: 7px; 
+    background-color: lightpink;
   }
 `
